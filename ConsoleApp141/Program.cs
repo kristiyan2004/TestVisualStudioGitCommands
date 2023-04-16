ed6936172691d66d70efeb2921694a5ce59ae17f
kristiyan2004 <124305449+kristiyan2004@users.noreply.github.com>
2023-04-16 18:34:36 +0300

    Lets keep only one statement again
    keeping one statement

--- ConsoleApp141/Program.cs
+++ ConsoleApp141/Program.cs
@@ -1,6 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World! This is new. This is added from GitHub");
-Console.WriteLine("Hello, World! This is new. This is added from GitHub");
-Console.WriteLine("Hello, World! This is new. This is added from GitHub");
-Console.WriteLine("Hello, World! This is new. This is added from GitHub");
-Console.WriteLine("Hello, World! This is new. This is added from GitHub");
